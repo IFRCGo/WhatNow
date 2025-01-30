ab2ecf49a34963ce58b8b9597105b94be365624a
LDFOUR\luisd <luisd@jazusoft.com>
2025-01-30 17:21:57 -0300

    WN-130, I added loading and accept term logic.

--- resources/assets/js/pages/legal_terms/index.vue
+++ resources/assets/js/pages/legal_terms/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="tc-page">
     <div class="tc-page-header d-flex justify-content-between align-items-center">
-      <h2 class="tc-page-title">Terms and Conditions</h2>
+      <h2 class="tc-page-title">{{ $t('terms_conditions.title') }}</h2>
       <div v-if="latestTerm && user && user?.data?.user_profile?.terms_version !== latestTerm.version">
-        <v-button :disabled="disabled" class="btn-primary btn btn-lg" @click="acceptTerms">
-          Accept
-        </v-button>
+        <b-button :disabled="disabled" class="btn-primary btn btn-lg" @click="acceptTerms">
+          {{ $t('terms_conditions.accept') }}	
+        </b-button>
       </div>
     </div>
     <div class="tc-container" @scroll="handleScroll">
-      <div v-if="latestTerm" v-html="markdown(latestTerm.content)"></div>
+      <div class="d-flex justify-content-center" v-if="loading">
+        <b-spinner type="grow" label="Loading..."></b-spinner>
+      </div>
+      <div v-if="latestTerm && !loading" v-html="markdown(latestTerm.content)"></div>
     </div>
   </div>
 </template>
@@ -18,27 +21,37 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  loading: false,
   mounted() {
     this.fetchLatestTerm();
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
   },
   methods: {
-    handleScroll(event) {
-      const container = event.target;
-      if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
+    handleScroll() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      if (scrollTop + windowHeight >= documentHeight) {
         this.disabled = false;
       }
     },
     async fetchLatestTerm() {
       try {
+        this.loading = true;
         await this.$store.dispatch('terms/fetchLatestTerms')
       } catch (e) {
         this.$noty.error(this.$t('error_alert_text'))
         console.error(e)
+      } finally {
+        this.loading = false;
       }
     },
     async acceptTerms () {
       try {
+        this.loading = true;
         const changes = {
         accepted_agreement: true
       }
@@ -46,12 +59,15 @@ export default {
       } catch (e) {
         this.$noty.error(this.$t('error_alert_text'))
         console.error(e)
+      } finally {
+        this.loading = false;
       }
     }
   },
   data() {
     return {
       disabled: true,
+      loading: false,
     };
   },
   computed: mapGetters({
@@ -62,32 +78,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '../../../sass/variables.scss';
 .tc-page {
   width: 80%;
   max-width: 1622px;
   margin: 0 auto;
 
+  .tc-page-header {
+    position: sticky;
+    top: 0;
+    background-color: $body-bg;
+  }
+
   .tc-page-title {
     font-size: 55px;
     font-weight: 600;
-    color: #1e1e1e;
+    color: $dark;
     margin: 33px 0 40px 0;
   }
 }
 .tc-container {
-  font-size: 20px;
-  height: 90vh;
-  overflow-y: scroll;
-
-  &::-webkit-scrollbar {
-    -webkit-appearance: none;
-    width: 10px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    border-radius: 5px;
-    background-color: rgba(0, 0, 0, .5);
-    -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, .5);
-  }
+  font-size: 14px;
 }
 </style>
