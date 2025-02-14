64b4dc3f974c230332b381585dfd3e0690ba42c4
Jose Ganora <jose.ganora@stoneridge.com>
2025-02-14 18:58:31 -0300

    #WN-114# Removed console.log

--- resources/assets/js/pages/api_stats/index.vue
+++ resources/assets/js/pages/api_stats/index.vue
@@ -126,7 +126,6 @@ export default {
       this.fetchCumulativeUsageStats()
     },
     selectedRegion () {
-      console.log(this.selectedRegion)
       this.fetchCumulativeUsageStats()
     },
     selectedLanguage () {
