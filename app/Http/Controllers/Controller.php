6a0ddf3e0eacaa6517f8e0d3cb595a01eeb15a27
LDFOUR\luisd <luisd@jazusoft.com>
2025-02-28 11:14:37 -0300

    WN-268 I installed l5-swagger and configure some endpoints.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,19 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\OpenApi(
+ *    @OA\Info(
+ *        title="Whatnow",
+ *        description="Whatnow front end API documentation",
+ *        version="1.0.0",
+ *    ),
+ *    @OA\Server(
+ *        url=L5_SWAGGER_CONST_HOST,
+ *        description="API Base URL"
+ *    )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
